e9ec851d2e8b495c3e2173c3c1b385636fb9a843
Alex <a.sendula@gmail.com>
2020-04-14 20:17:40 +0200

    optimized crontabHandler.py and test.py

--- temp/test.py
+++ temp/test.py
@@ -4,5 +4,3 @@ import requests
 def main():
     return requests.get(
         'https://api.telegram.org/bot1160110586:AAGq6npXmn839dWitkAvA-KSZYvisgdvFLc/sendMessage?text=test reply&chat_id=680658091')
-
-main()
